1b36728bd3a9314048a03028dc2fd3200e01c951
pglblb <107105622+tvbw@users.noreply.github.com>
2026-09-26 08:46:07 +0800

    Add files via upload

--- fullhd2.py
+++ fullhd2.py
@@ -51,107 +51,30 @@ class Spider(Spider):
         HOST = HOSTS[0]
 
     def _cats(self):
-        return [
-
-        {"type_id": "sites/vk-studio/", "type_name": "肛专"},
-        {"type_id": "categories/anal-orgasm/", "type_name": "肛射"},
-        {"type_id": "categories/anal/", "type_name": "休闲"},
-        {"type_id": "sites/glory-hole/", "type_name": "墙洞"},
-        {"type_id": "categories/gangbang/", "type_name": "群交"},
-        {"type_id": "sites/kink-classics/", "type_name": "群干"},
-        {"type_id": "categories/pov/", "type_name": "乳"},
-        {"type_id": "categories/horror/", "type_name": "恐"},
-        {"type_id": "sites/device-bondage/", "type_name": "虐"},
-        {"type_id": "categories/pregnant/", "type_name": "孕"}, 
-        {"type_id": "categories/19-years-old/", "type_name": "嫩"},
-        {"type_id": "categories/granny/", "type_name": "奶"},
-        {"type_id": "categories/grandpa/", "type_name": "爷"},
-        {"type_id": "categories/gagging", "type_name": "重"},
-        {"type_id": "categories/yoga", "type_name": "瑜伽"},
-        {"type_id": "categories/maid", "type_name": "女佣"},
-       {"type_id": "categories/rough-sex", "type_name": "粗暴"},
-        {"type_id": "categories/mmf/", "type_name": "群3"},
-        {"type_id": "categories/mmmf", "type_name": "群5"},
-       {"type_id": "categories/oiled/", "type_name": "推油"},
-        {"type_id": "sites/pure-mature/", "type_name": "追"}, 
-        {"type_id": "categories/ass-to-mouth", "type_name": "肛口"},
-       {"type_id": "categories/anal-play", "type_name": "肛游"},
-        {"type_id": "categories/first-anal", "type_name": "肛初"},
-        {"type_id": "categories/striptease", "type_name": "脱衣"},
-        {"type_id": "categories/mom", "type_name": "妈妈"},
-        {"type_id": "female-orgasm", "type_name": "高潮"},
-        {"type_id": "categories/cougar", "type_name": "徐娘"},
-        {"type_id": "categories/doctor", "type_name": "原画"},
-        {"type_id": "categories/jav-uncensored", "type_name": "日本"},
-        {"type_id": "categories/ffm", "type_name": "4人"},
-        {"type_id": "categories/cheating", "type_name": "出轨"},
-        {"type_id": "categories/threesome", "type_name": "三人"},
-        {"type_id": "categories/public", "type_name": "公共"},
-        {"type_id": "categories/swap", "type_name": "互换"},
-        {"type_id": "categories/cumplay", "type_name": "多人"},
-        {"type_id": "categories/stepmom", "type_name": "继母"},
-        {"type_id": "categories/police", "type_name": "警察"},
-        {"type_id": "categories/group-sex", "type_name": "集体"},
-        {"type_id": "categories/flogging", "type_name": "鞭打"},
-        {"type_id": "networks/brazzers-com/", "type_name": "Br"},
-        {"type_id": "networks/private/", "type_name": "PR"},
-        {"type_id": "networks/bangbros", "type_name": "Ba"},
-        {"type_id": "networks/blacked/", "type_name": "Bl"},
-        {"type_id": "networks/tushy-com/", "type_name": "Tu"},
-        {"type_id": "networks/mylf-com", "type_name": "My"},
-        {"type_id": "sites/my-dirty-maid/", "type_name": "Ma"},
-        {"type_id": "categories/shaved/", "type_name": "剃毛"},
-        {"type_id": "categories/slave/", "type_name": "奴"},
-        {"type_id": "categories/cumshot/", "type_name": "内"},
-        {"type_id": "categories/creampie/", "type_name": "体"},
-
-        {"type_id": "sites/fake-taxi/", "type_name": "车"}, 
-        {"type_id": "sites/passion-hd/", "type_name": "Pa"},
-        {"type_id": "sites/lubed/", "type_name": "Lu"},
-        {"type_id": "sites/tiny-4k/", "type_name": "T4K"},
-        {"type_id": "sites/exotic-4k/", "type_name": "E4K"},
-        {"type_id": "categories/jav-uncensored", "type_name": "JAV"},
-        {"type_id": "categories/beach/", "type_name": "Be"},
-        {"type_id": "networks/teamskeet-com", "type_name": "Tk"},
-        {"type_id": "networks/tushy-com", "type_name": "Tu"},
-        {"type_id": "networks/mofos-com", "type_name": "Mo"},
-        {"type_id": "networks/private", "type_name": "Pr"},
-        {"type_id": "networks/rk-com", "type_name": "Rk"},
-        {"type_id": "sites/latina-girlx/", "type_name": "拉丁"},
-        {"type_id": "sites/blacked/", "type_name": "黑白1"},
-        {"type_id": "categories/bbc-big-black-cock/", "type_name": "黑白2"},
-        {"type_id": "sites/stranded-teens/", "type_name": "外"},
-        {"type_id": "categories/double-penetration/", "type_name": "双1"},
-        {"type_id": "sites/dp-fanatics/", "type_name": "双2"},
-        {"type_id": "categories/ass-fingering/", "type_name": "指"}, 
-        {"type_id": "categories/fisting/", "type_name": "拳"},
-        {"type_id": "categories/squirt/", "type_name": "潮"},
-       {"type_id": "categories/double-pussy/", "type_name": "双"},
-       {"type_id": "categories/bbc-big-black-cock/", "type_name": "屌"},
-        {"type_id": "categories/rimming/", "type_name": "舔"},
-        {"type_id": "categories/pussy-licking/", "type_name": "屄"},
-        {"type_id": "sites/pure-mature/", "type_name": "白白"},
-        {"type_id": "categories/gaping/", "type_name": "洞口"},
-        {"type_id": "categories/shemale/", "type_name": "人妖"},
-        {"type_id": "categories/shemale-fuck-guy/", "type_name": "干男"},
-        {"type_id": "categories/shemale-fuck-shemale/", "type_name": "伪娘"},
-        {"type_id": "categories/asian/", "type_name": "亚洲"},
-        {"type_id": "categories/thai/", "type_name": "泰国"},
-        {"type_id": "categories/jav-uncensored/", "type_name": "日本"},
-        {"type_id": "categories/italian/", "type_name": "意哥"},
-        {"type_id": "categories/brazilian/", "type_name": "巴西"},   
-        {"type_id": "categories/mexican/", "type_name": "墨哥"},   
-        {"type_id": "categories/latina/", "type_name": "拉美"},   
-        {"type_id": "categories/hijab/", "type_name": "穆斯"},    
-        {"type_id": "categories/cfnm", "type_name": "cfnm"}, 
-        {"type_id": "networks/naughtyamerica-com", "type_name": "Naug🌠"},
-        {"type_id": "sites/sexmex", "type_name": "Sexmex🌠"},
-        {"type_id": "categories/animation", "type_name": "Animation🌠"},
-        {"type_id": "categories/18-years-old", "type_name": "Teen🌠"},
-        {"type_id": "categories/pawg", "type_name": "Pawg🌠"},
-        {"type_id": "categories/thong", "type_name": "Thong🌠"},
-        {"type_id": "categories/stockings", "type_name": "Stockings🌠"},
-        ]
+        global _CATS
+        if _CATS:
+            return _CATS
+        try:
+            r = self.fetch(f"{HOST}/zh/categories/", headers={"User-Agent": UA}, timeout=20000)
+            h = r.text if hasattr(r, 'text') else str(r)
+            cats, seen = [], set()
+            for m in re.finditer(r'href="[^"]*/zh/categories/([a-z0-9-]+)/"[^>]*title="([^"]+)"', h):
+                s, n = m.group(1), m.group(2).strip()
+                if s in seen or s in _SKIP:
+                    continue
+                seen.add(s)
+                if n.count('（') > n.count('）'):
+                    n = n.split('（')[0].strip()
+                if n.count('(') > n.count(')'):
+                    n = n.split('(')[0].strip()
+                if re.search(r'[\u3040-\u30ff]', n):
+                    n = s.replace('-', ' ').title()
+                if n:
+                    cats.append({"type_id": s, "type_name": n})
+            _CATS = cats[:40] or [{"type_id": "latest-updates", "type_name": "最新更新"}]
+        except:
+            _CATS = [{"type_id": "latest-updates", "type_name": "最新更新"}]
+        return _CATS
 
     def homeContent(self, filter=False):
         return {"class": self._cats(), "list": []}
